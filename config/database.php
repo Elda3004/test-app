bb3f6a537e05d2cd5ecacaefa428ce2734caac84
Elda Meco <elda.meco@hideolab.al>
2021-09-23 17:18:45 +0200

    config db connection

--- config/database.php
+++ config/database.php
@@ -98,9 +98,10 @@ return [
             'database' => env('MONGO_DB_DATABASE', 'laravel'),
             'username' => env('MONGO_DB_USERNAME', 'root'),
             'password' => env('MONGO_DB_PASSWORD', 'root'),
-            'options'  => [],
-            // 'dsn' => env('MONGO_DB_BASE_URI','mongodb://').env('MONGO_DB_USERNAME','root').':'.env('MONGO_DB_PASSWORD','root').env('MONGO_DB_HOST', 'localhost').env('MONGO_DB_DATABASE', 'laravel').'?retryWrites=true&w=majority',
-            'dsn' => 'mongodb+srv://elda-admin:cioyPvPUAoK6fSZ5@cluster0.jvp6l.mongodb.net/intelycare?retryWrites=true&w=majority'
+            'bas_uri'  => env('MONGO_DB_BASE_URI', 'mongodb+srv://'),
+            'dsn' => env('MONGO_DB_BASE_URI').env('MONGO_DB_USERNAME').':'.env('MONGO_DB_PASSWORD').env('MONGO_DB_HOST').'/'.env('MONGO_DB_DATABASE').'?retryWrites=true&w=majority'
+
+            // 'mongodb+srv://elda-admin:cioyPvPUAoK6fSZ5@cluster0.jvp6l.mongodb.net/intelycare?retryWrites=true&w=majority'
         ],
     ],
 
